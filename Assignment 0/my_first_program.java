543dad1e0d2f628d24feaa277d6e066490b2d1c4
msBurton <Mads@Madelines-MacBook-Pro.local>
2014-08-28 21:50:34 -0400

    Hello World Stub Program
    Use for first in class program.

---
+++ Assignment 0/my_first_program.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import static java.lang.System.*;
+
+import java.util.Scanner;
+
+class my_first_program{
+
+
+     public static void main (String str[]) throws IOException {
+
+         System.out.println ("Hello World"); 
+
+     }
+
+}
