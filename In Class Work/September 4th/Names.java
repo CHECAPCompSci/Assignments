bbade283ab0502b04c1fe0d6c509301ead712a9f
msBurton <Mads@Madelines-MacBook-Pro.local>
2014-09-03 21:14:50 -0400

    New Material for ch. 2

---
+++ In Class Work/September 4th/Names.java
@@ -0,0 +1,24 @@
+// ***************************************************************
+//   Names.java
+//
+//   Prints a list of student names with their hometowns
+//   and intended major
+// ***************************************************************
+
+public class Names
+{
+    // ------------------------
+    // main prints the list
+    // ------------------------
+    public static void main (String[] args)
+    {
+		
+		
+		
+		
+		
+    }
+}
+
+     
+
---
+++ In Class Work/September 4th/StudentGrades.java
@@ -0,0 +1,24 @@
+//********************************************************************
+//  StudentGrades.java
+//
+//  Prints a table of points students earned in lab work.
+//*******************************************************************
+
+public class StudentGrades
+{
+    //--------------------------------------------------------
+    // Print the table with a border and aligned columns;
+    // use the + operator to do arithmetic and concatenation.
+    //--------------------------------------------------------
+    public static void main (String[] args)
+    {
+		//Print Boarder
+		
+		
+		
+		//Print Table
+		
+		
+    }
+}
+
