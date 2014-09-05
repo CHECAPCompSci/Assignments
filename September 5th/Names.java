e2a237ae99f7c9bed1b929e062828651cc225163
Jeffrey Enamorado <jenamorado@ymail.com>
2014-09-05 09:37:34 -0400

    Names Java Work
    Make 3 collums Names, Hometown, and Intended major

--- September 5th/Names.java
+++ September 5th/Names.java
@@ -12,11 +12,15 @@ public class Names
     // ------------------------
     public static void main (String[] args)
     {
-		
-		
-		
-		
-		
+  
+      System.out.println ();
+System.out.println (" \tName\t\tHometown\tIntended Major") ;
+System.out.println ("\t====\t\t========");
+System.out.println ("\tJeffrey\t\tWashington Computer Engineer");
+System.out.println ("\tMilton\t\tWashington Mechanical Engineer");
+System.out.println ("\t Henry\t\tWashington Software Engineer");
+  
+  
     }
 }
 
