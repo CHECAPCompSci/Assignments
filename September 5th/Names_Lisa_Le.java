4b2fb846f2e77e5d8098f7b18dae963cf4d5d48d
Lisa Le <littleinuyasha123@gmail.com>
2014-09-05 10:02:22 -0400

    Names Completed

---
+++ September 5th/Names_Lisa_Le.java
@@ -0,0 +1,23 @@
+// ************************************************************ 
+// Names.java
+// 
+// Prints a list of student names with their hometowns 
+// and intended major
+// ************************************************************ 
+public class Names 
+{
+// -------------------------- 
+// main prints the list
+// -------------------------- 
+public static void main (String[] args) 
+{
+  System.out.println (); 
+  System.out.println ("\tName\t\tHometown\t\tCollege Major") ; 
+  System.out.println ("\tLisa Le\t\tDC\t\tUndecided"); 
+  System.out.println ("\tKatherine\t\tDC\t\tComputer Science and Cyber Security"); 
+  System.out.println ("\tNakia Green\tDC\t\tUndecided"); 
+  System.out.println ("\tJeffrey E.\t\tDC\t\tBusiness");
+ 
+  
+} 
+} 
