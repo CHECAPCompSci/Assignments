acc544d6692d98897037124b63a00a03f4455334
MiltonRaymundo <thewhiteboy0.0@gmail.com>
2014-09-05 09:41:24 -0400

    Names in Java
    Milton's Java Work

---
+++ September 5th/MiltonRaymundo.java
@@ -0,0 +1,19 @@
+public class Names
+{
+// --------------------------
+// main prints the list
+// --------------------------
+public static void main (String[] args)
+{
+System.out.println ();
+System.out.println (" \tName\t\tHometown\t\tMajor") ;
+System.out.println ("\t====\t\t========\t\t========");
+System.out.println ("\tMilton\t\tRaymundo\t\tMechanical Engineering");
+System.out.println ("\tDistrict of Columbia\tWashington");
+System.out.println ("\tFrancisco\t\tTorres\t\tAerospace");
+System.out.println ("\tEl Salvador\tCentral America");
+System.out.println ("\tHenry\t\tYu\t\tSoftware Engineer");
+System.out.println ("\tDistrict of Columbua\tWashington");
+System.out.println ();
+}
+}
