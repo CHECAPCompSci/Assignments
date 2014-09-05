709b9fd7dccb75411548acc09a23cc119e8b3715
Jeffrey Enamorado <jenamorado@ymail.com>
2014-09-05 10:03:52 -0400

    Unfinished Student Grades
    Finish the work.

--- September 5th/StudentGrades.java
+++ September 5th/StudentGrades.java
@@ -12,13 +12,17 @@ public class StudentGrades
     //--------------------------------------------------------
     public static void main (String[] args)
     {
-		//Print Boarder
-		
-		
-		
-		//Print Table
-		
-		
+  //Print Boarder
+   System.out.println (" ///////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+     System.out.println (" ==\tStudent Points\t       ==");
+       System.out.println (" \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\///////////////////");
+      
+  //Print Table
+       System.out.println ();
+          System.out.println ("Name\tLab\tBonus\tTotal");
+       System.out.println ("----\t---\t-----\t-----");
+      
+  
     }
 }
 
