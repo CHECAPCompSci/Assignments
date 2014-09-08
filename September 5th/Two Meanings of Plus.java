b471d2a54d66418e758b87492edcc98de2143dba
Lisa Le <littleinuyasha123@gmail.com>
2014-09-07 21:58:35 -0400

    Homework
    Student Grades & Ten Robins HW

---
+++ September 5th/Two Meanings of Plus.java
@@ -0,0 +1,5 @@
+//---------------------------------
+//Homework for September 05th, 2014
+//---------------------------------
+
+  System.out.println("Ten robins plus " + (10 + 3) + " canaries is " + 2 + 3 + " birds.")
