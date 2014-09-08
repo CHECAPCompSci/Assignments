c324c3cf17b74d0f5f3fc750e5b3bc3c96363349
franciscotorres <torres7991@gmail.com>
2014-09-08 19:51:25 -0400

    Finished two meanings of plus

---
+++ September 5th/Francisco_Torres_ two meanings of plus.java
@@ -0,0 +1,17 @@
+//**********************************************************
+//  Two Meanings of PLus.Java    Author: Torres, Franisco
+//
+//  Complete Part two of Two Meanings of Plus
+//**********************************************************
+
+public class Plus 
+{  
+  //==========================================================
+  // main prints the following statement
+  //==========================================================
+public static void main (String[] args)
+ {
+  System.out.println ("Ten robins plus " + 1 + 3 + " canaries is " + ( 10 + 13 ) + " birds.");   
+
+ }
+}
