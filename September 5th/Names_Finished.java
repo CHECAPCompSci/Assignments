2f9fa6d6a5cc751bdcd0e87b09b6bc63f5f990fc
franciscotorres <torres7991@gmail.com>
2014-09-08 20:05:39 -0400

    Completed Names Code

---
+++ September 5th/Names_Finished.java
@@ -0,0 +1,23 @@
+// ***************************************************************
+//   Names.java
+//
+//   Prints a list of student names with their hometowns
+//   and intended major
+// ***************************************************************
+
+public class Names
+{
+    // ------------------------
+    // main prints the list
+    // ------------------------
+    public static void main (String[] args)
+    {
+      System.out.println ();
+      System.out.println ("\tNames\t\tHometown\t\tIntended Major");
+      System.out.println ("\t=====\t\t========\t\t==============");
+      System.out.println ("\tFrancisco\t\tEl Salvador\tAerospace Engineering");
+      System.out.println ("\tMilton\t\tWashington DC\tMechanical Engineering");
+      System.out.println ("\tHenry\t\tWashginton DC\tSoftware Engineering");
+      System.out.println ("\tJeffrey\t\tWashginton DC\tComputer Engineering");
+    }
+}
